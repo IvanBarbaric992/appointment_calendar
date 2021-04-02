45010e2281cccdb7b581f03e080877d8ef08f30e
Ivan Barbarić <ivan.barbaric@netmedia.hr>
2021-04-02 12:08:11 +0200

    Add some footer with simple styiling

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { Typography } from '@material-ui/core';
+
+import './Footer.scss';
 
 const Footer = () => (
-  <div>
-    <></>
-  </div>
+  <footer className="footer--container">
+    <Typography variant="h5">Interesting calendar</Typography>
+  </footer>
 );
 
 export default Footer;
